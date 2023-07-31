8fdb12ad1621bf90f11742bb9af85b83f22dbf98
gooberville209 <128639935+gooberville209@users.noreply.github.com>
2023-07-31 10:39:05 -0400

    Create random.py

---
+++ random.py
@@ -0,0 +1,30 @@
+import random
+
+def guess_number():
+    # Randomly select a number between 1 and 100
+    number = random.randint(1, 100)
+    
+    guess = 0
+    attempts = 0
+    
+    print("I've chosen a number between 1 and 100. Try to guess it!")
+    
+    while guess != number:
+        try:
+            # Get the user's guess
+            guess = int(input("Enter your guess: "))
+            
+            # Check if the guess is correct
+            if guess < number:
+                print("Too low!")
+            elif guess > number:
+                print("Too high!")
+            
+            attempts += 1
+        except ValueError:
+            print("Please enter a valid number.")
+    
+    print(f"Congratulations! You've guessed the number {number} in {attempts} attempts.")
+
+# Run the game
+guess_number()
